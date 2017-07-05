4e39e1bb8ef0fcf659199e28d880431b26bb1a73
Brady Ericksen <bradyericksen@gmail.com>
2017-07-05 17:53:20 -0600

    Added beginnings of room blackouts.

---
+++ Transfer/Assets/Scripts/lightRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightRoom : MonoBehaviour {
+
+	public GameObject blackoutSpace;
+	//public GameObject blackoutSpaceExit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2d(Collider2D other){
+		if(other.tag == "Player")
+		{
+			//Instantiate(blackoutSpace,,); //instantiate 
+		}
+	}
+}
